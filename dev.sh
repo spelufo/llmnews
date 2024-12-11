c4150edc23b534486676b0274ea8d357899863d8
spelufo <santiagopelufo@gmail.com>
2024-12-11 07:51:37 -0300

    Deployment WIP.

--- dev.sh
+++ dev.sh
@@ -3,7 +3,18 @@
 set -e
 
 deploy() {
-  scp news.py root@pancho:/root/llmnews/news.py
+  if [[ -n $(git status --porcelain) ]]; then
+    read -p "There are uncommitted changes. Continue with deployment? [y/N] " response
+    if [[ ! $response =~ ^[Yy]$ ]]; then
+      echo "Deployment cancelled"
+      exit 1
+    fi
+  fi
+  deployment_name="deploy_$(date +%Y-%m-%d)_$(git rev-parse --short HEAD).tar.gz"
+  git archive --format=tar.gz HEAD > $deployment_name
+  scp $deployment_name root@pancho:/root/llmnews/$deployment_name
+  ssh root@pancho "cd /root/llmnews && tar xzf $deployment_name && rm $deployment_name"
+  mv $deployment_name deployments/
 }
 
 run() {
