7432b99a26cc2094dcc508f24a1d2f9895eb69ba
spelufo <santiagopelufo@gmail.com>
2024-12-11 08:15:55 -0300

    Load .env before running.

--- dev.sh
+++ dev.sh
@@ -3,22 +3,16 @@
 set -e
 
 deploy() {
-  if [[ -n $(git status --porcelain) ]]; then
-    read -p "There are uncommitted changes. Continue with deployment? [y/N] " response
-    if [[ ! $response =~ ^[Yy]$ ]]; then
-      echo "Deployment cancelled"
-      exit 1
-    fi
-  fi
-  deployment_name="deploy_$(date +%Y-%m-%d)_$(git rev-parse --short HEAD).tar.gz"
-  git archive --format=tar.gz HEAD > $deployment_name
-  scp $deployment_name root@pancho:/root/llmnews/$deployment_name
-  ssh root@pancho "cd /root/llmnews && tar xzf $deployment_name && rm $deployment_name"
-  mv $deployment_name deployments/
+  git push pancho main
+}
+
+pancho_git() {
+  ssh root@pancho "cd /root/llmnews && GIT_DIR=/root/llmnews.git GIT_WORK_TREE=/root/llmnews git $@"
 }
 
 run() {
-  python news.py
+  . .env
+  time python news.py
 }
 
 cmd="${1:-run}"
